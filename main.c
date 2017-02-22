1b9c9ae12b489a4300beac041d0885cb40ac970b
Zreast <zreast@hotmail.com>
2017-02-22 22:38:06 +0700

    init init func

--- main.c
+++ main.c
@@ -10,8 +10,16 @@ Computer Engineering KMITL
 
 #define BUFFER_SIZE 1000
 
+//buffer counter
+int buffer_counter = 0;
 int buffer[BUFFER_SIZE];
 int producer,consumer,size,request;
+
+pthread_mutex_t mutex;
+sem_t full,empty;
+
+pthread_t tid;
+pthread_attr_t attr;
 
 
 void *buffer_append(void *vargp)
@@ -28,10 +36,17 @@ void *buffer_remove(void *vargp)
 int main(void)
 {
   int i;
-  pthread_t tid;
 
   scanf("%d %d %d %d",&producer,&consumer,&size,&request);
-  buffer_size = size;
+
+  //init mutex lock
+  pthread_mutex_init(&mutex, NULL);
+  //init full semaphore to 0 , empty to buffer_size
+  sem_init(&full, 0, 0);
+  sem_init(&empty, 0, BUFFER_SIZE);
+
+  pthread_attr_init(&attr);
+
 
   for(i=1 ; i<=request ; i++)
   {
