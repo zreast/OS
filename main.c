1f73db681409a273164185611562eafab9f269d4
Zreast <zreast@hotmail.com>
2017-02-22 15:23:28 +0700

    init pthread library
    อือ

--- main.c
+++ main.c
@@ -5,11 +5,24 @@ Computer Engineering KMITL
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 int buffer[1000],N;
 
+void *testThread(void *vargp)
+{
+  sleep(1);
+  printf("This is thread\n");
+  return NULL;
+}
+
 int main(void)
 {
-  printf("t35t p21ntf\n");
+  pthread_t tid;
+  printf("t35t th23ad\n");
+  pthread_create(&tid , NULL , testThread , NULL);
+  pthread_join(tid , NULL);
+  printf("Exit thread\n");
   return 0;
 }
