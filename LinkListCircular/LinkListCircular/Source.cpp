8c5c2611b79547400330df10b5315d8c06b2c8f5
Millenium <pongaskm2007@gmail.com>
2017-02-23 00:05:31 +0700

    update circular link list

---
+++ LinkListCircular/LinkListCircular/Source.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#define max 1000
+//Global Variable
+struct node *head = NULL;
+struct node *current = NULL;
+int count = 0;
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+bool isEmpty() {
+	return head == NULL;
+}
+
+node* getNode(int num, node* nxt) {
+	node*q = (node*)malloc(sizeof(node));
+	q->data = num;
+	q->next = nxt;
+	return q;
+}
+
+void add(int num) {
+	struct node *temp = (struct node*) malloc(sizeof(struct node));
+	temp->data = num;
+
+	if (isEmpty()) {
+		head = temp;
+		head->next = head;
+		current = head;
+		count++;
+	}
+	else {
+		if (count == max) {
+			printf("It full shit!\n");
+		}
+		else {
+			current->next = temp;
+			temp->next = head;
+			current = temp;
+			count++;
+		}
+	}
+}
+
+void remove() {
+	
+	if (isEmpty()) {
+		printf("Fucking it empty\n");
+	}
+	else {
+		node* del = head;
+		head=head->next;
+		current->next = head;
+		free(del);
+		count--;
+	}
+}
+
+int main(void) {
+
+	for (int i = 0; i < 1100; i++) {
+		add(i); printf("%d\n", i);
+	}
+	for (int i = 0; i < 1100; i++) {
+		remove(); printf("%d\n", i);
+	}
+	remove();
+	remove();
+
+	//printf("%d", current->next->data);
+
+}
