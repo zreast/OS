16c522ad40a38277bed847cac5e0481e5421c1e1
Millenium <pongaskm2007@gmail.com>
2017-02-23 00:18:50 +0700

    Fix remove bug
    fuck buggie

--- LinkListCircular/LinkListCircular/Source.cpp
+++ LinkListCircular/LinkListCircular/Source.cpp
@@ -57,17 +57,33 @@ void remove() {
 		current->next = head;
 		free(del);
 		count--;
+		if (count == 0) {
+			head = NULL;
+			current = NULL;
+		}
 	}
 }
 
 int main(void) {
 
-	for (int i = 0; i < 1100; i++) {
-		add(i); printf("%d\n", i);
-	}
-	for (int i = 0; i < 1100; i++) {
-		remove(); printf("%d\n", i);
-	}
+	add(1);
+	add(2);
+	add(3);
+	add(4);
+	add(5);
+	add(6);
+	add(7);
+	add(8);
+	add(9);
+	add(10);
+	remove();
+	remove();
+	remove();
+	remove();
+	remove();
+	remove();
+	remove();
+	remove();
 	remove();
 	remove();
 
