dad7dce53f148bc209657c73674643f45379af65
Millenium <pongaskm2007@gmail.com>
2017-02-23 03:01:13 +0700

    Eiei banana

--- codeblock_project/main.c
+++ codeblock_project/main.c
@@ -2,10 +2,13 @@
 #include<stdlib.h>
 #include<conio.h>
 #define max 1000
+#include <pthread.h>
+#define NUM_THREADS	10
 //Global Variable
 struct node *head = NULL;
 struct node *current = NULL;
 int count = 0;
+pthread_mutex_t lock;
 
 typedef struct node
 {
@@ -65,7 +68,35 @@ void del() {
 	}
 }
 
+void *buffer_append(void *vargp)
+{
+   pthread_mutex_lock(&lock);
+   int tid;
+   tid = (int)vargp;
+   printf("buffer append, thread #%d!\n", tid);
+   add(tid);
+   pthread_mutex_unlock(&lock);
+   pthread_exit(NULL);
+}
+
+void *buffer_remove(void *vargp)
+{
+   pthread_mutex_lock(&lock);
+   int tid;
+   tid = (int)vargp;
+   printf("buffer remove, thread #%d!\n", tid);
+   del();
+   pthread_mutex_unlock(&lock);
+   pthread_exit(NULL);
+}
+
 int main(void) {
+    if (pthread_mutex_init(&lock, NULL) != 0)
+        {
+            printf("\n mutex init failed\n");
+            return 1;
+        }
+
 
 	add(1);
 	add(2);
@@ -78,6 +109,22 @@ int main(void) {
 	add(9);
 	add(10);
 
+
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   int t;
+   for(t=0;t<NUM_THREADS;t++){
+     printf("In main: creating thread %ld\n", t);
+     rc = pthread_create(&threads[t], NULL, buffer_append, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+       }
+     }
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
+   return 0;
 
 	//printf("%d", current->next->data);
 
