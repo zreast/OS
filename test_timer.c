14a733a1ee8f548d8c1fa728400c1f3004113ec2
Millenium <pongaskm2007@gmail.com>
2017-02-23 17:23:31 +0700

    """"

--- test_timer.c
+++ test_timer.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
-#define max 5
+#define max 10
 #include <pthread.h>
-#define NUM_THREADS	30
+#define NUM_THREADS	10
 #include <time.h>
 
 //Global Variable
@@ -18,11 +18,11 @@ typedef struct node
 	struct node* next;
 }node;
 
-int isEmpty() {
+int isEmpty() { //empty is 1
 	return head == NULL;
 }
 
-int isFull() {
+int isFull() { //full is 1
 	return count == max;
 }
 
@@ -42,12 +42,12 @@ int add(int num) {
 		head->next = head;
 		current = head;
 		count++;
-		return 0;
+		return 0;//Sucess to add
 	}
 	else {
 		if (count == max) {
 			printf("It full shit!\n");
-            return 1;
+            return 1; //It fail to add
 		}
 		else {
 			current->next = temp;
@@ -69,7 +69,7 @@ int del() {
 		node* del = head;
 		head=head->next;
 		current->next = head;
-		free(del);
+		free(del);//Love leak
 		count--;
 		if (count == 0) {
 			head = NULL;
@@ -84,18 +84,18 @@ void *buffer_append(void *vargp)  //add when not full
     double diff;
     int timeout;
     st = clock();
-    srand(time(NULL));
-    timeout = (rand()*(int)vargp)%5+1;
+    srand(time(NULL)); //Get system time
+    timeout = (rand()*(int)vargp)%5+1; //Random time out 1-5 sec
 
     while(isFull()==1){
         en = clock();
         diff = ((double)en-(double)st)/CLOCKS_PER_SEC;
-        if((int)diff>=timeout){
+        if((int)diff>=timeout){//Timeout
             printf("thread %d cannot append it time out\n",(int)vargp);
             pthread_exit(NULL);
         }
-
    }
+
    pthread_mutex_lock(&lock);
    int tid;
    tid = (int)vargp;
@@ -132,10 +132,10 @@ void *buffer_remove(void *vargp)
 
 int main(void) {
 
-     add(1);
-   add(2);
-   add(3);add(4);add(5);
-   add(6);
+  //    add(1);
+  //  add(2);
+  //  add(3);add(4);add(5);
+  //  add(6);
 
     if (pthread_mutex_init(&lock, NULL) != 0)
         {
@@ -156,14 +156,14 @@ int main(void) {
        }
      }
 
-     for(t=NUM_THREADS;t<NUM_THREADS+NUM_THREADS;t++){
-     printf("In main: creating thread %ld\n", t);
-     rc = pthread_create(&threads[t], NULL, buffer_remove, (void *)t);
-     if (rc){
-       printf("ERROR; return code from pthread_create() is %d\n", rc);
-       exit(-1);
-       }
-     }
+    for(t=NUM_THREADS;t<NUM_THREADS+NUM_THREADS;t++){
+        printf("In main: creating thread %ld\n", t);
+        rc = pthread_create(&threads[t], NULL, buffer_remove, (void *)t);
+        if (rc){
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+            }
+    }
 
 
    pthread_exit(NULL);
@@ -171,4 +171,3 @@ int main(void) {
 	//printf("%d", current->next->data);
 
 }
-
