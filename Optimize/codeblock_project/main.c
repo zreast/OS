a067a9889cfbe8e38977a0815e44dc27e032151e
Zreast <zreast@hotmail.com>
2017-02-24 16:03:14 +0700

    Clean Code

--- Optimize/codeblock_project/main.c
+++ Optimize/codeblock_project/main.c
@@ -12,9 +12,13 @@ int append_count = 0;
 int remove_count = 0;
 int error_count = 0, request_temp;
 volatile int running_threads = 0;
+
+//Mutex
 pthread_mutex_t running_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t append_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t remove_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+//Thread Array
 int temp_consumer_thread[500]={0};
 int temp_producer_thread[500]={0};
 
@@ -109,12 +113,12 @@ void *buffer_append(void *vargp)  //add when not full
     int timeout;
     do{
     if(request<0)break;
+
     pthread_mutex_lock(&append_mutex);
     append_count++;
     pthread_mutex_unlock(&append_mutex);
+
     st = clock();
-    //srand(time(NULL)); //Get system time
-    //timeout = (rand()*(int)vargp)%100+1; //Random time out 1-5 sec
 		timeout = ((int)vargp)%100+100;
 
     if(isFull()==1){
@@ -132,26 +136,27 @@ void *buffer_append(void *vargp)  //add when not full
        }
     }
     else{
-             pthread_mutex_lock(&lock);
-        int tid;
-        tid = (int)vargp;
+      pthread_mutex_lock(&lock);
+
+      int tid;
+      tid = (int)vargp;
        //printf("buffer append, thread #%d!\n", tid);
-       if(request<0){
+      if(request<0){
         running_threads--;  pthread_mutex_unlock(&lock);   pthread_exit(NULL);
-       }
-        add(tid);
-       //pthread_mutex_lock(&append_mutex);
-        append_count--;
-       //pthread_mutex_unlock(&append_mutex);
-       //pthread_mutex_lock(&running_mutex);
-       //pthread_mutex_unlock(&running_mutex);
-       request--;
-       pthread_mutex_unlock(&lock);
-       temp_producer_thread[tid]=0;
+      }
+      add(tid);
+      append_count--;
+      request--;
+
+      pthread_mutex_unlock(&lock);
+      temp_producer_thread[tid]=0;
     }
 
-    }while(request>0);
-    running_threads--;
+    }
+
+		while(request>0);
+    	running_threads--;
+
    pthread_exit(NULL);
 }
 
@@ -163,13 +168,14 @@ void *buffer_remove(void *vargp)
     int timeout;
     do{
     if(request<0)break;
+
     pthread_mutex_lock(&remove_mutex);
     remove_count++;
     pthread_mutex_unlock(&remove_mutex);
+
     st = clock();
-    //srand(time(NULL));
-    //timeout = (rand()*(int)vargp)%100+1;
 		timeout=((int)vargp)%100+100;
+
     if(isEmpty()==1){
         while(isEmpty()==1){
         en = clock();
@@ -185,29 +191,30 @@ void *buffer_remove(void *vargp)
    }
     }
     else{
-     pthread_mutex_lock(&lock);
-   int tid;
-   tid = (int)vargp;
-   //printf("buffer remove, thread #%d!\n", tid);
-       if(request<0){
-         running_threads--;  pthread_mutex_unlock(&lock);  pthread_exit(NULL);
+    	pthread_mutex_lock(&lock);
+		  int tid;
+		  tid = (int)vargp;
+   		//printf("buffer remove, thread #%d!\n", tid);
+      if(request<0){
+        running_threads--;  pthread_mutex_unlock(&lock);  pthread_exit(NULL);
       }
-   del();
-    //pthread_mutex_lock(&remove_mutex);
-       remove_count--;
-    //pthread_mutex_unlock(&remove_mutex);
-    //pthread_mutex_lock(&running_mutex);
-   //pthread_mutex_unlock(&running_mutex);
+
+   		del();
+      remove_count--;
       request--;
-   pthread_mutex_unlock(&lock);
-   temp_consumer_thread[tid]=0;
+   		pthread_mutex_unlock(&lock);
+   		temp_consumer_thread[tid]=0;
     }
-   }while(request>0);
-   running_threads--;
+   }
+	 while(request>0);
+   		running_threads--;
+
    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
+
+	// Bug Catch
 	if(argc != 5){
 		printf("Parameter not correct\n");
 		return 1;
@@ -231,151 +238,47 @@ int main(int argc, char *argv[]){
   int rc;
   int t,i,j;
 
-    srand(time(NULL));
+  srand(time(NULL));
 	//Start time
 	initial=clock();
-    request_temp = request;
+  request_temp = request;
 
-    for(i=0;i<producer;i++){
-                if(temp_producer_thread[i]==0){
-                    pthread_create(&threads[i], NULL, buffer_append, (void *)i);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     //request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_producer_thread[i]=1;
+	// PHASE 1
 
-                }
-        }
-        for(j=producer;j<(producer+consumer);j++){
-                if(temp_consumer_thread[i]==0){
-                    pthread_create(&threads[j], NULL, buffer_remove, (void *)j);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     //request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_consumer_thread[j]=1;
-                }
-        }
-          while(request>0){
-           // Sleep(1);
-           //printf("running thread is %d\n",running_threads);
-          //printf("\rrequest is %d",request);
-       }
-    /*
-    while(1){
+  for(i=0;i<producer;i++){
+    if(temp_producer_thread[i]==0){
+        pthread_create(&threads[i], NULL, buffer_append, (void *)i);
+        pthread_mutex_lock(&running_mutex);
+         running_threads++;
+         //request--;
+        pthread_mutex_unlock(&running_mutex);
+        temp_producer_thread[i]=1;
+    }
+  }
 
-        for(i=0;i<producer/4;i++){
-                if(temp_producer_thread[i]==0){
-                    pthread_create(&threads[i], NULL, buffer_append, (void *)i);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_producer_thread[i]=1;
+	// PHASE 2
 
-                }
-                if(request<=0)break;
-        }
-        for(j=producer;j<(producer+consumer)/4;j++){
-                if(temp_consumer_thread[i]==0){
-                    pthread_create(&threads[j], NULL, buffer_remove, (void *)j);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_consumer_thread[j]=1;
-                }
-                if(request<=0)break;
-        }
-        /////////////////////////////////////////////////////////////////////////////////
-        for(i=producer/4;i<producer/2;i++){
-                if(temp_producer_thread[i]==0){
-                    pthread_create(&threads[i], NULL, buffer_append, (void *)i);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_producer_thread[i]=1;
-
-                }
-                if(request<=0)break;
-        }
-        for(j=(producer+consumer)/4;j<(producer+consumer)/2;j++){
-                if(temp_consumer_thread[i]==0){
-                    pthread_create(&threads[j], NULL, buffer_remove, (void *)j);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_consumer_thread[j]=1;
-                }
-                if(request<=0)break;
-        }
-        ////////////////////////////dsfafadfsasdfds//////////////////
-        ////////////////////////////asdfsasdsadsfdsdaafsd//////////
-         for(i=producer/2;i<producer*3/4;i++){
-                if(temp_producer_thread[i]==0){
-                    pthread_create(&threads[i], NULL, buffer_append, (void *)i);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_producer_thread[i]=1;
-
-                }
-                if(request<=0)break;
-        }
-        for(j=producer/2;j<(producer+consumer)*3/4;j++){
-                if(temp_consumer_thread[i]==0){
-                    pthread_create(&threads[j], NULL, buffer_remove, (void *)j);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_consumer_thread[j]=1;
-                }
-                if(request<=0)break;
-        }
-        /////////////////////////////////////////////////////////////////////////////////
-        for(i=producer*3/4;i<producer;i++){
-                if(temp_producer_thread[i]==0){
-                    pthread_create(&threads[i], NULL, buffer_append, (void *)i);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_producer_thread[i]=1;
-
-                }
-                if(request<=0)break;
-        }
-        for(j=(producer+consumer)*3/4;j<producer+consumer;j++){
-                if(temp_consumer_thread[i]==0){
-                    pthread_create(&threads[j], NULL, buffer_remove, (void *)j);
-                    pthread_mutex_lock(&running_mutex);
-                     running_threads++;
-                     request--;
-                    pthread_mutex_unlock(&running_mutex);
-                    temp_consumer_thread[j]=1;
-                }
-                if(request<=0)break;
-        }
-
-         while(running_threads>0){
-            Sleep(5);
-            }
-
-           if(request<=0)break;
-           printf("request is %d\n",request);
-    }*/
+  for(j=producer;j<(producer+consumer);j++){
+    if(temp_consumer_thread[i]==0){
+        pthread_create(&threads[j], NULL, buffer_remove, (void *)j);
+        pthread_mutex_lock(&running_mutex);
+         running_threads++;
+         //request--;
+        pthread_mutex_unlock(&running_mutex);
+        temp_consumer_thread[j]=1;
+    }
+  }
+  while(request>0){
+       // Sleep(1);
+       //printf("running thread is %d\n",running_threads);
+      //printf("\rrequest is %d",request);
+  }
 
 	//Stop time
 	final=clock();
 	temp_cpu = ((double)final-(double)initial) / CLOCKS_PER_SEC ;
 
 	//Outputs
-	//printf("Error count = %d\n",error_count);
 	printf("\nSuccessfully consumed %d requests (%.2f%%)\n",request_temp-error_count,((double)(request_temp-error_count)/(double)request_temp)* 100);
 	printf("Elapsed Time: %.2f s\n",temp_cpu);
 	printf("Throughput: %.2f successful requests/s\n",(double)(request_temp-error_count)/temp_cpu);
@@ -383,6 +286,5 @@ int main(int argc, char *argv[]){
   //Last thing that main() should do
   pthread_exit(NULL);
   return 0;
-
 
 }
